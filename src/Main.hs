f675043d72ccd62e612b47d9ff0ffe4217f16860
thomkoehler <thom.koehler@gmx.com>
2014-07-15 14:41:39 +0200

    GpxTool

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,15 @@
 
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import XmlParser
 
+import Filesystem.Path.CurrentOS(fromText)
 
 main :: IO ()
 main = do
-   route <- parseFile "Route.gpx"
+   route <- parseFile $ fromText "Route1.gpx"
    print route
    return ()
 
--- src/XmlParser.hs
+++ src/XmlParser.hs
@@ -10,6 +10,7 @@ import Data.Maybe(fromMaybe)
 import Text.XML
 import Text.Printf(printf)
 import Filesystem.Path.CurrentOS(FilePath)
+import Data.List(foldl')
 
 import qualified Data.Map as M
 
@@ -28,10 +29,19 @@ gpxName localName = Name localName (Just "http://www.topografix.com/GPX/1/1") No
    
 
 parseRoute :: Element -> Route
-parseRoute = Route . elemAttr (gpxName "name")
+parseRoute routeElem = 
+   Route 
+   { 
+      rteName = lookupChildContent (gpxName "name") routeElem, 
+      rtePoints = [] 
+   }
 
 parseGpx :: Element -> Gpx
-parseGpx = Gpx . parseRoute . lookupElement1 (gpxName "rte")
+parseGpx gpxElem = 
+   Gpx
+   { 
+      gpxRoute = parseRoute $ lookupChildElement1 (gpxName "rte") gpxElem
+   }
 
 
 elemAttr :: Name -> Element -> Text
@@ -42,22 +52,37 @@ elemAttr name elem =
       fromMaybe (error msg) $ M.lookup name $ elementAttributes elem
       
       
-lookupElements :: Name -> Element -> [Element]
-lookupElements name elem =
-   map nodeToElement $ filter filterFun $ elementNodes elem
+lookupChildElements :: Name -> Element -> [Element]
+lookupChildElements name parentElem =
+   map nodeToElement $ filter filterFun $ elementNodes parentElem
    where
       nodeToElement (NodeElement childElem) = childElem
+      nodeToElement _ = undefined
    
       filterFun (NodeElement childElem) = elementName childElem == name
       filterFun _ = False
 
-      
-lookupElement1 :: Name -> Element -> Element
-lookupElement1 name elem = case lookupElements name elem of
-   []      -> error $ printf "Element %s not found." $ show name
-   [child] -> child
+   
+lookupChildElement1 :: Name -> Element -> Element
+lookupChildElement1  name parentElem = 
+   fromMaybe (error $ printf "Element %s not found." (show name)) $ lookupChildElement name parentElem
+
+
+lookupChildElement :: Name -> Element -> Maybe Element
+lookupChildElement name parentElem = case lookupChildElements name parentElem of
+   []      -> Nothing
+   [child] -> Just child
    _       -> error $ printf "More than one Element %s found." $ show name
 
-   
+
+lookupChildContent :: Name -> Element -> Maybe Text
+lookupChildContent name parentElem = do
+   child <- lookupChildElement name parentElem
+   lookupContent child
+   where
+      lookupContent (Element _ _ [NodeContent content]) = Just content
+      lookupContent  _ = Nothing
+
+      
 
 -----------------------------------------------------------------------------------------------------------------------
